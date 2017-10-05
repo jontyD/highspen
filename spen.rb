cb74323b8e3ab1d44fcc57e42c560afb9cbaa20a
Jonty Davis <jonathan.davis@transcendit.co.uk>
2017-10-05 15:14:46 +0100

    more generic method that will deal with all routes

--- spen.rb
+++ spen.rb
@@ -4,6 +4,13 @@ require 'sinatra'
 
 configure { set :server, :puma }
 
-get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+get '/*' do
+  viewname = params[:splat].first   # eg "some/path/here"
+
+  if File.exist?(File.join(settings.public_folder, "#{viewname}.html"))
+      send_file File.join(settings.public_folder, "#{viewname}.html")
+  else
+    "Sorry.... I can't find that page."
+  end
+
 end
